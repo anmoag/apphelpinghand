94d203cdf0676ea2f5ed5f1cb16934cf621645f1
anmoag <anmoag5@gmail.com>
2023-05-22 10:52:17 +0200

    Big Bang

---
+++ src/main/java/com/inserta/apphelpinghand/ApphelpinghandApplication.java
@@ -0,0 +1,13 @@
+package com.inserta.apphelpinghand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApphelpinghandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApphelpinghandApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/sql/appHelping.sql
@@ -0,0 +1,85 @@
+-- Borrar la base de datos si existe
+DROP DATABASE IF EXISTS appHelping;
+
+-- Crear la base de datos
+CREATE DATABASE appHelping;
+
+-- Usar la base de datos
+USE appHelping;
+
+-- Crear tabla usuarios
+CREATE TABLE usuarios (
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT, -- El rango de valores posibles para un BIGINT generalmente
+                          -- va desde -9223372036854775808 hasta 9223372036854775807.
+                          id_tipo INT NOT NULL,
+                          email VARCHAR(255) NOT NULL UNIQUE,
+                          foto VARCHAR(255),
+                          latitud DOUBLE NOT NULL,
+                          longitud DOUBLE NOT NULL,
+                          pass VARCHAR(255) NOT NULL,
+                          fecha_nacimiento DATETIME,
+                          usuario_desde DATETIME NOT NULL,
+                          token VARCHAR(255),
+                          sexo VARCHAR(255),
+                          nombre VARCHAR(255) NOT NULL,
+                          apellido1 VARCHAR(255),
+                          apellido2 VARCHAR(255),
+                          curso VARCHAR(255),
+                          direccion VARCHAR(255) NOT NULL,
+                          ciudad VARCHAR(255) NOT NULL,
+                          codigo_postal INT NOT NULL,
+                          pais VARCHAR(255) NOT NULL,
+                          telefono VARCHAR(255),
+                          logueado BOOLEAN
+);
+
+-- Crear tabla mensajes
+CREATE TABLE mensajes (
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          contenido VARCHAR(255) NOT NULL,
+                          remite_id BIGINT NOT NULL,
+                          destinatario_id BIGINT NOT NULL,
+                          fecha_envio DATETIME NOT NULL,
+                          leido BOOLEAN,
+                          FOREIGN KEY (remite_id) REFERENCES usuarios(id),
+                          FOREIGN KEY (destinatario_id) REFERENCES usuarios(id)
+);
+
+-- Crear tabla chat_mensajes
+CREATE TABLE chat_mensajes (
+                           id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                           remitente_id BIGINT NOT NULL,
+                           destinatario_id BIGINT NOT NULL,
+                           contenido VARCHAR(255) NOT NULL,
+                           fecha_envio DATETIME NOT NULL,
+                           FOREIGN KEY (remitente_id) REFERENCES usuarios(id),
+                           FOREIGN KEY (destinatario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE puntuaciones (
+                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            usuario_acosado_id BIGINT NOT NULL,
+                            usuario_ayuda_id BIGINT NOT NULL,
+                            puntuacion INT NOT NULL,
+                            FOREIGN KEY (usuario_acosado_id) REFERENCES usuarios (id),
+                            FOREIGN KEY (usuario_ayuda_id) REFERENCES usuarios (id)
+);
+
+CREATE TABLE incidencias (
+                             id INT PRIMARY KEY AUTO_INCREMENT,
+                             descripcion VARCHAR(255) NOT NULL,
+                             fecha_hora DATETIME NOT NULL,
+                             usuario_id INT,
+                             estado VARCHAR(20),
+                             prioridad VARCHAR(20),
+                             comentarios TEXT,
+                             FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE historial_acciones (
+                            id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                            usuario_id BIGINT NOT NULL,
+                            accion VARCHAR(255) NOT NULL,
+                            fecha DATETIME NOT NULL,
+                            FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);

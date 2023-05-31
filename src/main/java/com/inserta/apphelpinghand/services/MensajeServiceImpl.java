a5212d44d0ab94ab35305c9e3da7dd0b69c022db
anmoag <anmoag5@gmail.com>
2023-05-31 17:51:31 +0200

    Añadida documentación en MensajeServiceImpl y insertado 10 usuarios en la base de datos

--- src/main/java/com/inserta/apphelpinghand/services/MensajeServiceImpl.java
+++ src/main/java/com/inserta/apphelpinghand/services/MensajeServiceImpl.java
@@ -21,7 +21,11 @@ public class MensajeServiceImpl implements MensajeService {
         this.usuarioRepo = usuarioRepo;
 
     }
-
+    /**
+     * Envía un mensaje de ayuda desde un usuario acosado a los usuarios cercanos.
+     * @param usuarioAcosado El usuario acosado que envía el mensaje de ayuda.
+     * @return El mensaje enviado.
+     */
     public Mensaje enviarMensajeAcosado(Usuario usuarioAcosado) {
 
         // Crear mensaje
@@ -36,12 +40,19 @@ public class MensajeServiceImpl implements MensajeService {
         for (Usuario usuario : this.obtenerUsuariosCercanos(usuarioAcosado, RANGO_DE_INFLUENCIA)) {
             mensaje.setDestinatario(usuario);
             mensajeRepo.save(mensaje);
+            System.out.println(mensaje);
         }
         return mensaje;
 
     }
 
-    //Método que proviene de stack overflow para calcular la distancia entre dos Usuarios
+    /**
+     * Calcula la distancia en metros entre dos usuarios basándose en sus coordenadas geográficas.
+     * Utiliza la fórmula del haversine para el cálculo.
+     * @param usuarioAcosado El usuario acosado.
+     * @param usuarioCercano El usuario cercano.
+     * @return La distancia en metros entre los dos usuarios.
+     */
     public double calcularDistancia(Usuario usuarioAcosado, Usuario usuarioCercano) {
         double latitud1Rad = Math.toRadians(usuarioAcosado.getLatitud());
         double latitud2Rad = Math.toRadians(usuarioCercano.getLatitud());
@@ -61,6 +72,13 @@ public class MensajeServiceImpl implements MensajeService {
         return distanciaEnMetros;
     }
 
+    /**
+     * Obtiene una lista de usuarios cercanos a un usuario acosado, dentro de una distancia específica.
+     * Los usuarios cercanos se determinan en función de su distancia geográfica utilizando el método `calcularDistancia`.
+     * @param usuarioAcosado El usuario acosado.
+     * @param distancia La distancia máxima en la que se considera que un usuario está "cerca" del usuario acosado.
+     * @return Una lista de usuarios cercanos al usuario acosado, dentro de la distancia especificada.
+     */
     public List<Usuario> obtenerUsuariosCercanos(Usuario usuarioAcosado, double distancia) {
         List<Usuario> todosLosUsuarios = usuarioRepo.findAll();
         List<Usuario>usuariosCercanos = new ArrayList<>();

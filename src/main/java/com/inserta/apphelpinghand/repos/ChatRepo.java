1018bcc1ec1d32972169300bf67171a000c897d5
enriquemolina15 <molinfernandez15@gmail.com>
2023-05-31 17:39:06 +0200

    Cambio data.sql,se ha realizado varios insert a cada tabla. En el README he puesto un enlace  para postman de chatmensaje. He creado el servicio y el controlador para chatMensaje y he creado un metodo para buscar los mensajes por fecha.

--- src/main/java/com/inserta/apphelpinghand/repos/ChatRepo.java
+++ src/main/java/com/inserta/apphelpinghand/repos/ChatRepo.java
@@ -5,8 +5,14 @@ import com.inserta.apphelpinghand.models.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 @RepositoryRestResource(path = "chats")
 
 public interface ChatRepo extends JpaRepository<Chat, Long> {
+    public List<Chat> findAllByFechaEnvio(LocalDateTime fechaHora);
+    public Boolean deleteChatById(int id);
 }
--- src/main/resources/sql/squema.sql
+++ src/main/resources/sql/squema.sql
@@ -11,7 +11,7 @@ USE appHelping;
 -- Crear tabla usuarios
 CREATE TABLE usuarios (
                           id BIGINT PRIMARY KEY AUTO_INCREMENT, -- El rango de valores posibles para un BIGINT generalmente
-                          -- va desde -9223372036854775808 hasta 9223372036854775807.
+    -- va desde -9223372036854775808 hasta 9223372036854775807.
                           id_tipo INT NOT NULL,
                           email VARCHAR(255) NOT NULL UNIQUE,
                           foto VARCHAR(255),
@@ -48,22 +48,22 @@ CREATE TABLE mensajes (
 
 -- Crear tabla chat_mensajes
 CREATE TABLE chat_mensajes (
-                           id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                           remitente_id BIGINT NOT NULL,
-                           destinatario_id BIGINT NOT NULL,
-                           contenido VARCHAR(255) NOT NULL,
-                           fecha_envio DATETIME NOT NULL,
-                           FOREIGN KEY (remitente_id) REFERENCES usuarios(id),
-                           FOREIGN KEY (destinatario_id) REFERENCES usuarios(id)
+                               id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                               remitente_id BIGINT NOT NULL,
+                               destinatario_id BIGINT NOT NULL,
+                               contenido VARCHAR(255) NOT NULL,
+                               fecha_envio DATETIME NOT NULL,
+                               FOREIGN KEY (remitente_id) REFERENCES usuarios(id),
+                               FOREIGN KEY (destinatario_id) REFERENCES usuarios(id)
 );
 
 CREATE TABLE puntuaciones (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            usuario_acosado_id BIGINT NOT NULL,
-                            usuario_ayuda_id BIGINT NOT NULL,
-                            puntuacion INT NOT NULL,
-                            FOREIGN KEY (usuario_acosado_id) REFERENCES usuarios (id),
-                            FOREIGN KEY (usuario_ayuda_id) REFERENCES usuarios (id)
+                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                              usuario_acosado_id BIGINT NOT NULL,
+                              usuario_ayuda_id BIGINT NOT NULL,
+                              puntuacion INT NOT NULL,
+                              FOREIGN KEY (usuario_acosado_id) REFERENCES usuarios (id),
+                              FOREIGN KEY (usuario_ayuda_id) REFERENCES usuarios (id)
 );
 
 CREATE TABLE incidencias (
@@ -73,37 +73,20 @@ CREATE TABLE incidencias (
                              usuario_id BIGINT,
                              estado VARCHAR(20),
                              prioridad VARCHAR(20),
-                             comentarios TEXT,-- Algunos han tenido que poner un TINYTEXT
+                             comentarios TINYTEXT,-- Algunos han tenido que poner un TINYTEXT
 
                              FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
 CREATE TABLE historial_acciones (
-                            id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                            usuario_id BIGINT NOT NULL,
-                            accion VARCHAR(255) NOT NULL,
-                            fecha DATETIME NOT NULL,
-                            FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+                                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                    usuario_id BIGINT NOT NULL,
+                                    accion VARCHAR(255) NOT NULL,
+                                    fecha DATETIME NOT NULL,
+                                    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
 CREATE TABLE palabras (
                           id INT AUTO_INCREMENT PRIMARY KEY,
                           palabra VARCHAR(255)
 );
-INSERT INTO palabras (palabra) VALUES ('gilipollas');
-INSERT INTO palabras (palabra) VALUES ('imbécil');
-INSERT INTO palabras (palabra) VALUES ('cabrón');
-INSERT INTO palabras (palabra) VALUES ('cabrona');
-INSERT INTO palabras (palabra) VALUES ('Subnormal');
-INSERT INTO palabras (palabra) VALUES ('hijoputa');
-INSERT INTO palabras (palabra) VALUES ('hijaputa');
-INSERT INTO palabras (palabra) VALUES ('hijo de puta');
-INSERT INTO palabras (palabra) VALUES ('hija de puta');
-INSERT INTO palabras (palabra) VALUES ('tonto');
-INSERT INTO palabras (palabra) VALUES ('tonta');
-INSERT INTO palabras (palabra) VALUES ('puta');
-INSERT INTO palabras (palabra) VALUES ('puto');
-INSERT INTO palabras (palabra) VALUES ('capullo');
-INSERT INTO palabras (palabra) VALUES ('capulla');
-INSERT INTO palabras (palabra) VALUES ('payaso');
-INSERT INTO palabras (palabra) VALUES ('payasa');

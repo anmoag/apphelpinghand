5112e68fde138a771dc944c7f4f025511bdca4e6
anmoag <anmoag5@gmail.com>
2023-06-03 09:19:54 +0200

    Servicios y controlador de la clase mensaje funcionales

--- src/main/java/com/inserta/apphelpinghand/service/MensajeService.java
+++ src/main/java/com/inserta/apphelpinghand/service/MensajeService.java
@@ -2,11 +2,10 @@ package com.inserta.apphelpinghand.service;
 
 import com.inserta.apphelpinghand.models.Mensaje;
 import com.inserta.apphelpinghand.models.Usuario;
-
 import java.util.List;
 
 public interface MensajeService {
     Mensaje enviarMensajeAcosado(Usuario usuarioAcosado);
     double calcularDistancia(Usuario usuarioAcosado, Usuario usuarioCercano);
-    public List<Usuario> obtenerUsuariosCercanos(Usuario usuarioAcosado, double distancia);
+    List<Usuario> obtenerUsuariosCercanos(Usuario usuarioAcosado, double distancia);
 }
--- src/main/java/com/inserta/apphelpinghand/service/MensajeServiceImpl.java
+++ src/main/java/com/inserta/apphelpinghand/service/MensajeServiceImpl.java
@@ -2,7 +2,6 @@ package com.inserta.apphelpinghand.service;
 
 import com.inserta.apphelpinghand.models.Mensaje;
 import com.inserta.apphelpinghand.models.Usuario;
-import com.inserta.apphelpinghand.repos.MensajeRepo;
 import com.inserta.apphelpinghand.repos.UsuarioRepo;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -11,13 +10,12 @@ import java.util.List;
 
 @Service
 public class MensajeServiceImpl implements MensajeService {
-    private final MensajeRepo mensajeRepo;
+
     private final UsuarioRepo usuarioRepo;
     private static final double RADIO_DE_LA_TIERRA = 6371; // Radio de la Tierra en kilómetros
-    private static final double RANGO_DE_INFLUENCIA = 300;
+    private static final double RANGO_DE_INFLUENCIA = 6371000; //pongo el maximo de influencia para que coja todos los Usuarios
 
-    public MensajeServiceImpl(MensajeRepo messageRepo, UsuarioRepo usuarioRepo) {
-        this.mensajeRepo = messageRepo;
+    public MensajeServiceImpl(UsuarioRepo usuarioRepo) {
         this.usuarioRepo = usuarioRepo;
 
     }
@@ -27,7 +25,6 @@ public class MensajeServiceImpl implements MensajeService {
      * @return El mensaje enviado.
      */
     public Mensaje enviarMensajeAcosado(Usuario usuarioAcosado) {
-
         // Crear mensaje
         Mensaje mensaje = new Mensaje();
         mensaje.setContenido("Necesito ayuda");
@@ -35,12 +32,9 @@ public class MensajeServiceImpl implements MensajeService {
         mensaje.setFechaEnvio(LocalDateTime.now()); // Dar formato a la fecha y hora
         usuarioAcosado.setFoto("foto1"); // Aquí la url de la foto del usuario
         mensaje.setLeido(false);
-
         // Enviar mensaje a usuarios cercanos
         for (Usuario usuario : this.obtenerUsuariosCercanos(usuarioAcosado, RANGO_DE_INFLUENCIA)) {
             mensaje.setDestinatario(usuario);
-            mensajeRepo.save(mensaje);
-            System.out.println(mensaje);
         }
         return mensaje;
 
@@ -58,17 +52,13 @@ public class MensajeServiceImpl implements MensajeService {
         double latitud2Rad = Math.toRadians(usuarioCercano.getLatitud());
         double deltaLatitud = Math.toRadians(usuarioCercano.getLatitud() - usuarioAcosado.getLatitud());
         double deltaLongitud = Math.toRadians(usuarioCercano.getLongitud() - usuarioAcosado.getLatitud());
-
         double a = Math.sin(deltaLatitud / 2) * Math.sin(deltaLatitud / 2)
                 + Math.cos(latitud1Rad) * Math.cos(latitud2Rad)
                 * Math.sin(deltaLongitud / 2) * Math.sin(deltaLongitud / 2);
-
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
         double distanciaEnMetros;
         // Convertir la distancia a metros
         distanciaEnMetros = RADIO_DE_LA_TIERRA * c * 1000;
-
         return distanciaEnMetros;
     }
 
@@ -82,16 +72,14 @@ public class MensajeServiceImpl implements MensajeService {
     public List<Usuario> obtenerUsuariosCercanos(Usuario usuarioAcosado, double distancia) {
         List<Usuario> todosLosUsuarios = usuarioRepo.findAll();
         List<Usuario>usuariosCercanos = new ArrayList<>();
-
-        for (Usuario usuario : todosLosUsuarios) {
-            if (usuario.getId() != usuarioAcosado.getId()) { // no incluyo al usuarioAcosado
-                double distanciaEntreUsuarios = calcularDistancia(usuarioAcosado, usuario);
+        for (Usuario usuarioCercano : todosLosUsuarios) {
+            if (usuarioCercano.getId() != usuarioAcosado.getId()) { // no incluyo al usuarioAcosado
+                double distanciaEntreUsuarios = calcularDistancia(usuarioAcosado, usuarioCercano);
                 if (distanciaEntreUsuarios <= distancia) {
-                    usuariosCercanos.add(usuario);
+                    usuariosCercanos.add(usuarioCercano);
                 }
             }
         }
-
         return usuariosCercanos;
     }
 }

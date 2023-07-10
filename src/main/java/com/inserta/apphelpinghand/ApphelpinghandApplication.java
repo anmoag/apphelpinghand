dbf5827cc7dc978c0696eb2bca6956a6fbc77c56
enriquemolina15 <molinafernandez15@gmail.com>
2023-07-10 12:16:45 +0200

    Configurando Cors

--- src/main/java/com/inserta/apphelpinghand/ApphelpinghandApplication.java
+++ src/main/java/com/inserta/apphelpinghand/ApphelpinghandApplication.java
@@ -2,8 +2,10 @@ package com.inserta.apphelpinghand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.inserta.apphelpinghand.config")
 public class ApphelpinghandApplication {
 
     public static void main(String[] args) {

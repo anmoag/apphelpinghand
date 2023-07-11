a884882f9c3d241d63ddc89d0dae172223bf0af2
enriquemolina15 <molinafernandez15@gmail.com>
2023-07-11 10:27:39 +0200

    Configurando Cors y usuarioRepo

--- src/main/java/com/inserta/apphelpinghand/ApphelpinghandApplication.java
+++ src/main/java/com/inserta/apphelpinghand/ApphelpinghandApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.inserta.apphelpinghand.config")
 public class ApphelpinghandApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/inserta/apphelpinghand/repos/UsuarioRepo.java
+++ src/main/java/com/inserta/apphelpinghand/repos/UsuarioRepo.java
@@ -5,10 +5,11 @@ import com.inserta.apphelpinghand.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @Repository
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepo extends JpaRepository<Usuario, Long> {
